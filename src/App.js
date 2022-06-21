2d7a313513a78c40f41ff865959c0ffce2a38f81
mj0107 <alswns1285@gmail.com>
2022-06-22 06:41:16 +0900

    CRUD 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
 import './App.css';
+import { useState, useRef } from 'react'
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "kmj1",
+//     content: "hi",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "kmj2",
+//     content: "hi",
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "kmj3",
+//     content: "hi",
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    }
+    dataId.current++;
+    // 원래 데이터에 newItem을 이어붙여서 새로 setData를 해줌
+    setData([newItem, ...data]);
+  }
+
+  const onRemove = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다`);
+    const newDiaryList = data.filter((item) => item.id !== targetId);
+    setData(newDiaryList);
+  }
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((item) => 
+        item.id === targetId ? {...item, content:newContent} : it)
+    );
+  }
+
   return (
     <div className="App">
-      <DiaryEditor />
+      <DiaryEditor onCreate={onCreate}/>
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
 }
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-const DiaryEditor = () => {
+const DiaryEditor = ({onCreate}) => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   // state는 setState 함수가 아니면 절대로 변화시킬 수가 없음
   const [state, setState] = useState({
     author: "",
@@ -16,19 +19,34 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log(state);
+    if(state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+
+    if(state.content.length < 5) {
+      contentInput.current.focus();
+      return;
+    }
+
+    onCreate(state.author, state.content, state.emotion);
     alert('저장 성공');
+    setState({
+      author: "",
+      content: "",
+      emotion: 1,
+    });
   }
   
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
-        <input name="author" value={state.author} onChange={handleChangeState} />
+        <input ref={authorInput} name="author" value={state.author} onChange={handleChangeState} />
       </div>
 
       <div>
-        <textarea name="content" value={state.content} onChange={handleChangeState}/>
+        <textarea ref={contentInput} name="content" value={state.content} onChange={handleChangeState}/>
       </div>
 
       <div>
